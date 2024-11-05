924e11d2bdb18965153825472ea50fa8111e04a3
Brandon Presley <brandon@presley.nz>
2024-11-05 17:23:54 +1300

    First commit

---
+++ lesson1.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  print('I just said something');
+  print('And its written in the console');
+  print('Now ill write a number');
+  print(42);
+  
+  // This is a comment! It's just plain english
+  // and doesn't impact the code.
+  
+  // Below this line print out some of your own statements.
+}
---
+++ lesson2.dart
@@ -0,0 +1,11 @@
+void main(List<String> arguments) {
+  print('Strings are different to numbers...');
+  
+  // Fix this line.
+  print('Hello' + 42);
+  
+  var age = 50;
+  var daysLived = age * 365;
+  
+  print('At $age you lived $daysLived days.');
+}
